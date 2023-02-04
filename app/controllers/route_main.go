edb7d314d277e102f0ef6263fcc32226903c044e
NaokiIgarashi <igarashinaoki0504@gmail.com>
2023-02-04 14:39:35 +0900

    htmlの共通化

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -1,18 +1,9 @@
 package controllers
 
 import (
-	"html/template"
-	"log"
 	"net/http"
 )
 
 func top(w http.ResponseWriter, r *http.Request) {
-	// templateの構造体を生成
-	t, err := template.ParseFiles("app/views/templates/top.html")
-
-	// エラーハンドリング
-	if err != nil {
-		log.Panicln(err)
-	}
-	t.Execute(w, "hello")
+	generateHTML(w, "Hello", "layout", "top")
 }
--- app/controllers/server.go
+++ app/controllers/server.go
@@ -1,12 +1,33 @@
 package controllers
 
 import (
+	"fmt"
+	"html/template"
 	"net/http"
 	"todo_app/config"
 )
 
+func generateHTML(w http.ResponseWriter, data interface{}, filenames ...string) {
+	// filesを定義
+	var files []string
+
+	// filenamesを回す
+	for _, file := range filenames {
+		// filesに代入
+		files = append(files, fmt.Sprintf("app/views/templates/%s.html", file))
+	}
+
+	// Mustはあらかじめtemplateをキャッシュしておいて効率良くする。ParseFilesは失敗の際にパニック状態になる。
+	template := template.Must(template.ParseFiles(files...))
+
+	// defineを使用している場合は、ExecuteTemplateを使用して明示的に宣言する必要がある
+	template.ExecuteTemplate(w, "layout", data)
+}
+
 func StartMainServer() error {
 	files := http.FileServer(http.Dir(config.Config.Static))
+
+	// app/view内には動的ファイルが入る可能性があるため、静的ファイルであることを明示するためにstaticを使用
 	http.Handle("/static/", http.StripPrefix("/static/", files))
 
 	// URLの登録（「/」に訪れたらtopを表示する）
